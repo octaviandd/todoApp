db3c21a4495bcdfbc017e16b4b9e713a537be0d2
octaviandd <octaviandd@yahoo.com>
2020-02-15 11:33:37 +0000

    update

--- webpack.config.js
+++ webpack.config.js
@@ -15,15 +15,13 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-object-rest-spread','@babel/plugin-proposal-class-properties']
-          }
-        }
-      }
+            plugins: [
+              '@babel/plugin-proposal-object-rest-spread',
+              '@babel/plugin-proposal-class-properties',
+            ],
+          },
+        },
+      },
     ],
-  }
-<<<<<<< HEAD
-  
+  },
 };
-=======
-};
->>>>>>> 2dc636e8615d563a1000eb5f40a7523bb961425c
